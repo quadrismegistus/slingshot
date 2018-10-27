e93a92174d021839851fd25bab11cd95c191782b
quadrismegistus <ryan.heuser@gmail.com>
2018-10-26 23:05:34 -0700

    ** * *** * ** *

--- slings/prosodic_parser.py
+++ slings/prosodic_parser.py
@@ -160,18 +160,17 @@ def chadwyck_xml_to_txt(xml_string, OK=['l','lb'], BAD=['note'], body_tag='poem'
 	num_lines=0
 	if not num_stanzas:
 		for line in dom('l'):
-			if line_lim and len(txt)>=line_lim: break
 			txt+=[line.text.strip()]
 			num_lines+=1
-			if num_lines>=line_lim: break
+			if line_lim and num_lines>=line_lim: break
 	else:
 		for stanza in dom('stanza'):
 			if line_lim and len(txt)>=line_lim: break
 			for line in stanza('l'):
 				txt+=[line.text.strip()]
 				num_lines+=1
-				if num_lines>=line_lim: break
-			if num_lines>=line_lim: break
+				if line_lim and num_lines>=line_lim: break
+			if line_lim and num_lines>=line_lim: break
 			txt+=['']
 
 	txt='\n'.join(txt).replace(u'∣','').strip()
