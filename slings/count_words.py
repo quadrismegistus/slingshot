e8dbe1557d8af99458059851d5db87c90fdb5f5e
quadrismegistus <ryan.heuser@gmail.com>
2018-10-28 16:02:52 -0700

    adding support for R slings

--- slings/count_words.py
+++ slings/count_words.py
@@ -11,10 +11,8 @@ def count_words_fast(path):
 	from future_builtins import map
 	from collections import Counter
 	from itertools import chain
-
-	def countInFile(filename):
-		with open(filename) as f:
-			return Counter(chain.from_iterable(map(tokenize_fast, f)))
+	with codecs.open(path,encoding='utf-8') as f:
+		return Counter(chain.from_iterable(map(tokenize_fast, f)))
 
 def tokenize_fast(line):
 	return re.findall("[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+",line).lower()
