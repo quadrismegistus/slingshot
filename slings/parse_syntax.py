5310328096ad607481b19caa5e65bd899a178281
quadrismegistus <ryan.heuser@gmail.com>
2018-12-15 17:06:21 -0800

    fixing tsv path lists

--- slings/parse_syntax.py
+++ slings/parse_syntax.py
@@ -28,15 +28,20 @@ def parse_path(path):
 	return list(parse(path2txt(path)))
 
 # Main parser
+import time
 def parse(txt,max_len=MAX_LEN):
 	txt=txt.replace('\r\n','\n').replace('\r','\n')
 	paras=txt.split('\n\n')
 	#doc=nlp(txt)
+	now=time.time()
+	num_tokens=0
 	for pi,para in enumerate(paras):
+		if not pi%10: print '>>',pi,'...'
 		para=para.strip()
 		if len(para)>max_len: continue
 		doc=nlp(para)
 		for token in doc:
+			num_tokens+=1
 			td={'word':token.text,
 				'lemma':token.lemma_,
 				'tag':token.tag_,
@@ -52,7 +57,10 @@ def parse(txt,max_len=MAX_LEN):
 				#'children':'|'.join([child.text for child in token.children])
 			   }
 			yield td
-
+	nownow=time.time()
+	duration=nownow-now
+	rate=num_tokens/duration
+	print '>> FINISHED PROCESSING IN %s seconds (%s wps)' % (round(duration,1),rate)
 
 """
 Appendices
