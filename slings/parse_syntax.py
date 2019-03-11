e63efbaa7493f9ed4aa07edecda59113c88f8ca7
quadrismegistus <ryan.heuser@gmail.com>
2019-03-11 10:06:33 -0700

    changing caching system

--- slings/parse_syntax.py
+++ slings/parse_syntax.py
@@ -87,7 +87,6 @@ def postprocess_iter(results_jsonl_fn,only_words=set(),only_pos=set(),only_rels=
 	if only_pos: only_pos=set(only_pos)
 	if only_rels: only_rels=set(only_rels)
 	for ipath,(path,data) in enumerate(stream_results(results_jsonl_fn)):
-		if not ipath%100000: print '>>',ipath,path,'...'
 		if limit and ipath>=limit: break
 		if '.ipynb' in path: continue
 		sent_ld=[]
@@ -102,6 +101,7 @@ def postprocess_iter(results_jsonl_fn,only_words=set(),only_pos=set(),only_rels=
 					odx['_i']=wnum
 					odx['num_sent']=num_sent
 					odx['fn']=fn
+					if not wnum%100000: print '>>',ipath,path,odx,'...'
 					yield odx
 					sent_ld=[]
 			sent_ld+=[dx]
